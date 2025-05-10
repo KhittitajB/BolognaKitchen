27b35aff5d03aa75972c71b39ae8ddd584f16103
Khittitaj BUNUPURADAH <khittitaj.b@ku,th>
2025-05-11 03:12:35 +0700

    I seriously have no idea what I've done

--- configs.py
+++ configs.py
@@ -9,19 +9,19 @@ BUTTON_COLOR = (50, 200, 50)
 BUTTON_HOVER = (30, 180, 30)
 BUTTON_RECT = pygame.Rect(WIDTH // 2 - 50, HEIGHT - 60, 100, 40)
 
-meat_img = pygame.image.load("BolognaKitchen/assets/minecraft_pork.png")
-veggie_img = pygame.image.load("BolognaKitchen/assets/minecraft_carrot.png")
-grains_img = pygame.image.load("BolognaKitchen/assets/minecraft_wheat.png")
-dairy_img = pygame.image.load("BolognaKitchen/assets/minecraft_milk.png")
+meat_img = pygame.image.load("assets/minecraft_pork.png")
+veggie_img = pygame.image.load("assets/minecraft_carrot.png")
+grains_img = pygame.image.load("assets/minecraft_wheat.png")
+dairy_img = pygame.image.load("assets/minecraft_milk.png")
 
-background = pygame.image.load("BolognaKitchen/assets/kitchen.png")
+background = pygame.image.load("assets/kitchen.png")
 
 meat_img = pygame.transform.scale(meat_img, (CARD_WIDTH, CARD_HEIGHT))
 veggie_img = pygame.transform.scale(veggie_img, (CARD_WIDTH, CARD_HEIGHT))
 grains_img = pygame.transform.scale(grains_img, (CARD_WIDTH, CARD_HEIGHT))
 dairy_img = pygame.transform.scale(dairy_img, (CARD_WIDTH, CARD_HEIGHT))
 
-draw_pile_img = pygame.image.load("BolognaKitchen/assets/card_back.png")
+draw_pile_img = pygame.image.load("assets/card_back.png")
 draw_pile_img = pygame.transform.scale(draw_pile_img, (CARD_WIDTH, CARD_HEIGHT))
 
 SUITE_IMAGES = {
